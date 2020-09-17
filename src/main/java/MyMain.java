7fb20f5870c4b4d4e9a0b952b8f1091e696f31a5
Reevak05 <kaveergera@gmail.com>
2020-09-17 00:54:43 +0000

    add code

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,12 +3,27 @@ import java.util.Scanner;
 public class MyMain {
     
     public static int binaryToDecimal(String binary) { 
-        return -1;
-        // REPLACE WITH YOUR CODE HERE
+        int total = 0;
+        int input = Integer.parseInt(binary);
+        int power = 1;
+        while (input>0) {
+            total += (input%10)*power;
+            power *= 2;
+            input /= 10;
+        }
+        return total;
     }
     
     
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        boolean cont = true;
+        while (cont){
+            System.out.println("Please enter a number in binary to be converted to base 10: ");
+            System.out.println("That equals "+binaryToDecimal(scan.next())+" in base 10. Would like to convert another number?");
+            if (!scan.next().equals("yes")) cont = false;           
+        }
+        System.out.println("Goodbye.");
+        scan.close();
     }
 }
